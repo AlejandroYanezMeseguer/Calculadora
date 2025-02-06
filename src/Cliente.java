e04f68a6b90990633af6417e8754be3d42467d12
bascomias <ayanezmeseguer@danielcastelao.org>
2025-02-06 14:46:54 +0100

    pacasa

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,7 +1,4 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.util.Scanner;
@@ -9,6 +6,7 @@ import java.util.Scanner;
 public class Cliente {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String[] ArrayOperacion = new String[3];
         try {
             // InetSocketAddress nos permite encapsular dirección y puerto en un único punto
             // En caso de que nos sea útil, pero necesitamos una línea más
@@ -19,30 +17,35 @@ public class Cliente {
             socket.connect(dir);
             System.out.println("Conectado al servidor");
 
-            OutputStream salida = socket.getOutputStream();
-
-            PrintWriter escritor = new PrintWriter(salida,true);
+            ObjectOutputStream salida = new ObjectOutputStream(socket.getOutputStream());
 
             System.out.println("Introduce el primer operando");
-            float Op1 = sc.nextFloat();
-            sc.nextLine(); // Consumir la nueva línea
-            escritor.println(Op1);
+            String Op1 = sc.nextLine();
+          //  sc.nextLine(); // Consumir la nueva línea
+            ArrayOperacion[0] = Op1;
 
             System.out.println("Introduce el segundo operando");
-            float Op2 = sc.nextFloat();
-            sc.nextLine(); // Consumir la nueva línea
-            escritor.println(Op2);
+            String Op2 = sc.nextLine();
+           // sc.nextLine(); // Consumir la nueva línea
+            ArrayOperacion[1] = Op2;
 
             System.out.println("Introduce el simbolo de la operacion");
             String Operacion = sc.nextLine();
-            escritor.println(Operacion);
+            ArrayOperacion[2] = Operacion;
+            salida.writeObject(ArrayOperacion);
+
+            for (int i = 0; i < 3; i++) {
+
+                System.out.println(ArrayOperacion[i]);
+
+            }
 
 
-            // Obtenemos el flujo
-            BufferedReader lector = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-            String mensaje = lector.readLine();
-            System.out.println("Servidor dice: " + mensaje);
+//            // Obtenemos el flujo
+//            BufferedReader lector = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+//
+//            String mensaje = lector.readLine();
+//            System.out.println("Servidor dice: " + mensaje);
 
             socket.close();
         } catch (Exception e) {
--- src/Servidor.java
+++ src/Servidor.java
@@ -1,47 +1,44 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Servidor {
     public static void main(String[] args) {
-        while (true){
-            try {
-                // InetSocketAddress nos permite encapsular dirección y puerto en un único punto
-                // En caso de que nos sea útil, pero necesitamos una línea más
-                // que utilizando directamente el constructor del socket
-                InetSocketAddress dir = new InetSocketAddress("localhost", 6666);
-                ServerSocket servidor = new ServerSocket();
-                servidor.bind(dir);
+        try {
+            // InetSocketAddress nos permite encapsular dirección y puerto en un único punto
+            // En caso de que nos sea útil, pero necesitamos una línea más
+            // que utilizando directamente el constructor del socket
+            InetSocketAddress dir = new InetSocketAddress("localhost", 6666);
+            ServerSocket servidor = new ServerSocket();
+            servidor.bind(dir);
 
-                System.out.println("Esperando conexiones...");
-                Socket socket = servidor.accept();
-                System.out.println("Cliente conectado");
+            System.out.println("Esperando conexiones...");
+            Socket socket = servidor.accept();
+            System.out.println("Cliente conectado");
 
-                BufferedReader lector = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            ObjectInputStream lector = new ObjectInputStream(socket.getInputStream());
 
-                for (int i = 0; i < 3; i++) {
-                    // Aquí no se avanzará hasta que NO haya una recepción de mensaje
-                    String mensaje = lector.readLine();
-                    System.out.println("Cliente dice: " + mensaje);
-                }
+            // Aquí no se avanzará hasta que NO haya una recepción de mensaje
+            String[] mensaje = (String[]) lector.readObject();
 
-                OutputStream salida = socket.getOutputStream();
-                // AutoFlush = true para que envíe los datos inmediatamente
-                // para vaciar el buffer de PrintWriter
-                PrintWriter escritor = new PrintWriter(salida, true);
-                // Enviamos el mensaje al cliente
+            for (int i = 0; i < 3; i++) {
 
+                System.out.println(mensaje[i]);
 
-                socket.close();
-                servidor.close();
-            } catch (Exception e) {
-                e.printStackTrace();
             }
-        }
 
+            OutputStream salida = socket.getOutputStream();
+            // AutoFlush = true para que envíe los datos inmediatamente
+            // para vaciar el buffer de PrintWriter
+            PrintWriter escritor = new PrintWriter(salida, true);
+            // Enviamos el mensaje al cliente
+
+
+            socket.close();
+            servidor.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
