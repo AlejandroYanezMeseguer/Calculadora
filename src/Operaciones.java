2c7d70e5883be0f24bc1e1d124b007974ac9b61b
bascomias <ayanezmeseguer@danielcastelao.org>
2025-02-06 09:12:00 +0100

    Operaciones de la calculadora

---
+++ src/Operaciones.java
@@ -0,0 +1,33 @@
+public class Operaciones {
+
+    public static int Suma(int Operando1, int Operando2){
+
+        int resutado = Operando1 + Operando2;
+
+        return resutado;
+    }
+
+    public static int Resta(int Operando1, int Operando2){
+
+        int resutado = Operando1 - Operando2;
+
+        return resutado;
+    }
+
+    public static int Multiplicacion(int Operando1, int Operando2){
+
+        int resutado = Operando1 * Operando2;
+
+        return resutado;
+    }
+
+    public static int Division(int Operando1, int Operando2){
+
+        int resutado = Operando1 / Operando2;
+
+        return resutado;
+    }
+
+
+}
+
