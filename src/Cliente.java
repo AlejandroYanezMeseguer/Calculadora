50b0c30617e20fb5fce17cce9d9d7b7cf281e0e5
bascomias <ayanezmeseguer@danielcastelao.org>
2025-02-06 09:15:27 +0100

    Codigo generico cliente servidor reciclado de otro ejercicio

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,5 +1,42 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Cliente {
     public static void main(String[] args) {
+        int puerto_servidor = 6666;
+        byte[] bufferEnvio = new byte[2048];
+        byte[] bufferRecepcion = new byte[2048];
+        Scanner sc = new Scanner(System.in);
+        try {
+            InetAddress direccionServidor = InetAddress.getByName("localhost");
 
+            DatagramSocket datagramSocket = new DatagramSocket();
+            String msj;
+            System.out.println("Introduce el mensaje para el servidor");
+            msj = sc.nextLine();
+            bufferEnvio = msj.getBytes();
+            DatagramPacket pregunta = new DatagramPacket(bufferEnvio, bufferEnvio.length, direccionServidor, puerto_servidor);
+            datagramSocket.send(pregunta);
+
+            DatagramPacket respuesta = new DatagramPacket(bufferRecepcion, bufferRecepcion.length);
+            datagramSocket.receive(respuesta);
+            System.out.println("Recibo respuesta del servidor");
+
+            String msjServidor = new String(respuesta.getData(), 0, respuesta.getLength());
+            System.out.println("msjServidor = " + msjServidor);
+        } catch (SocketException ex) {
+            ex.printStackTrace();
+        } catch (UnknownHostException ex) {
+            Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
--- src/Servidor.java
+++ src/Servidor.java
@@ -1,5 +1,43 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
 public class Servidor {
     public static void main(String[] args) {
+        int puerto = 6666;
+        byte[] buffer = new byte[2048];
+        try {
+            System.out.println("Servidor arrancando");
+            DatagramSocket datagramSocket = new DatagramSocket(puerto);
 
+            DatagramPacket peticion = new DatagramPacket(buffer, buffer.length);
+            datagramSocket.receive(peticion);
+            System.out.println("Recibida peticion");
+
+            String msj = new String(peticion.getData(), 0, peticion.getLength());
+            System.out.println("msj = " + msj);
+
+            String[] palabras = msj.split(" ");
+            String palabraMasLarga = "";
+            for (String palabra : palabras) {
+                if (palabra.length() > palabraMasLarga.length()) {
+                    palabraMasLarga = palabra;
+                }
+            }
+
+            String respuesta = "Palabra más larga: " + palabraMasLarga + ", Longitud: " + palabraMasLarga.length();
+            buffer = respuesta.getBytes();
+            InetAddress direccion = peticion.getAddress();
+            int puertoCliente = peticion.getPort();
+            DatagramPacket respuestaPacket = new DatagramPacket(buffer, buffer.length, direccion, puertoCliente);
+            datagramSocket.send(respuestaPacket);
+            System.out.println("Envié respuesta al cliente");
+        } catch (SocketException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 }
